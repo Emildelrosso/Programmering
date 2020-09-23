9a27e70affa0f69bf31c409982bb39afca37b67a
Emil <emil.delrosso@elev.ga.ntig.se>
2020-09-23 13:32:09 +0200

    uppgifter

---
+++ HelloWorld/loopar.js
@@ -0,0 +1,5 @@
+let teachers = ["Niklas", "Victor", "Parvin"]
+
+for (let i = teachers.length - 1; i >= 0 ; i--) {
+    console.log(teachers[i])
+}
--- HelloWorld/variabler.js
+++
@@ -1,6 +0,0 @@
-let name = "Emil"
-
-let age = 17
-
-console.log("Hejsan mitt namn är " + name + " och jag är " + age + " år gammal")
-
