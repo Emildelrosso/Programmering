860cba4a242dd4744c7de69f916fe5e8c3aedf8b
Emil <emil.delrosso@elev.ga.ntig.se>
2020-10-16 12:59:36 +0200

    hej

--- Kattis/dice.js
+++ Kattis/dice.js
@@ -1,4 +1,3 @@
-const { listeners } = require('process');
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -13,15 +12,19 @@ function code(){
     let side2 = Number(lines[1])
     let side3 = Number(lines[2])
     let side4 = Number(lines[3])
+    let side5 = Number(lines[4])
+    let side6 = Number(lines[5])
+    let side7 = Number(lines[6])
+    let side8 = Number(lines[7])
 
-    let dice1 = (side1 + side2) / 2
-    let dice2 = (side3 + side4) / 2
+    let dice1 = (side1 + side2) / 2 + (side3 + side4) / 2
+    let dice2 = (side5 + side6) / 2 + (side7 + side8) / 2
     
     if (dice1 > dice2) {
-        console.log("Emma Won!");
+        console.log("Emma Won with " + dice1 + " points!");
     }
     else if (dice2 > dice1) {
-        console.log("Gunnar Won!");
+        console.log("Gunnar Won with " + dice2 + " points!");
     }
     else if (dice1 == dice2) {
         console.log("It's a tie!");
@@ -30,7 +33,7 @@ function code(){
 
   rl.on('line', (input) => {
       lines.push(input)
-        if (lines.length == 4){
+        if (lines.length == 8){
             code(); {
                 rl.close()
             }
