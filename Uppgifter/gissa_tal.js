38c94af84be8f06027b98189714a91de5cbfe1cd
Emil <emil.delrosso@elev.ga.ntig.se>
2020-10-23 13:20:08 +0200

    gissa talet

--- Uppgifter/gissa_tal.js
+++ Uppgifter/gissa_tal.js
@@ -5,17 +5,30 @@ const rl = readline.createInterface({
     output: process.stdout
   });
 
- rl.question("Guess a number between 1 and 10: ", (answer) => {
   const num = Math.floor((Math.random() * 10) + 1)
-  console.log(answer);
-  
-    if (answer == num)
+
+  let tries = 5 
+
+  rl.on('line', (input) => {
+  let answer = parseInt(input)
+
+  tries--;
+    if (tries == 0) {
+      console.log("You are our of tires!")
+      process.exit();
+    }
+
+    if (answer == num) {
       console.log("You guessed the right number!")
+      process.exit();
+    }
+   
+    else if (answer > num) {
+      console.log("Try a lower number!")
+    }
 
-    else if (answer > num)
-      console.log("Try a lower number.")
-
-    else if (answer < num)
-      console.log("Try a higher number:")
-
-})
+    else if (answer < num) {
+      console.log("Try a higher number!")
+    }
+    
+  })
